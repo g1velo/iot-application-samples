0b5e2f3499b62e7f0e66e0b12042e355b1c007b8
phariprasadreddy <phariprasadreddy@in.ibm.com>
2016-09-16 15:33:16 +0530

    Java sample application changes to use client libraries with HTTP(S) rest api support

--- java/standalone-samples/src/main/java/com/ibm/iotf/sample/client/application/HttpCommandPublish.java
+++ java/standalone-samples/src/main/java/com/ibm/iotf/sample/client/application/HttpCommandPublish.java
@@ -1,6 +1,6 @@
 /**
  *****************************************************************************
- * Copyright (c) 2015-16 IBM Corporation and other Contributors.
+ * Copyright (c) 2016 IBM Corporation and other Contributors.
 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,8 +8,8 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- * Sathiskumar Palaniappan - Initial Contribution
  * Amit M Mangalvedkar - Initial Contribution
+ * Sathiskumar Palaniappan - Initial Contribution 
  *****************************************************************************
  */
 
@@ -18,32 +18,34 @@ package com.ibm.iotf.sample.client.application;
 import java.io.IOException;
 import java.util.Properties;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.ibm.iotf.client.api.APIClient;
-import com.ibm.iotf.client.app.ApplicationClient;
+import com.ibm.iotf.client.device.DeviceClient;
 import com.ibm.iotf.sample.client.SystemObject;
 
 /**
  * 
- * This sample shows how an application publish a device event 
- * using HTTP(s) to IBM Watson IoT Platform on behalf of the device.
+ * This sample shows how an application publish commands to device
+ * using HTTP(s) to IBM Watson IoT Platform
  *
  */
-public class HttpApplicationDeviceEventPublish {
-
+public class HttpCommandPublish {
+	
 	private final static String PROPERTIES_FILE_NAME = "/application.properties";
 
 	public static void main(String[] args) throws Exception {
+		
 		/**
 		  * Load device properties
 		  */
 		Properties props = new Properties();
 		try {
-			props.load(HttpApplicationDeviceEventPublish.class.getResourceAsStream(PROPERTIES_FILE_NAME));
+			props.load(HttpCommandPublish.class.getResourceAsStream(PROPERTIES_FILE_NAME));
 		} catch (IOException e1) {
 			System.err.println("Not able to read the properties file, exiting..");
 			System.exit(-1);
-		}
+		}		
 		
 		APIClient myClient = null;
 		try {
@@ -55,41 +57,24 @@ public class HttpApplicationDeviceEventPublish {
 		}
 		
 		SystemObject obj = new SystemObject();
-		/**
-		 * Publishes this process load event for every 5 second
-		 */
-		while(true) {
-			boolean code = false;
-			try {
-				
-				//Generate a JSON object of the event to be published
-				JsonObject event = new JsonObject();
-				event.addProperty("name", SystemObject.getName());
-				event.addProperty("cpu",  obj.getProcessCpuLoad());
-				event.addProperty("mem",  obj.getMemoryUsed());
-				
-				// publish the event on behalf of device
-				code = myClient.publishApplicationEventforDeviceOverHTTP("blink", event);
-			
-				Thread.sleep(1000);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			
-			if(code == true) {
-				System.out.println("Published the event successfully !");
-			} else {
-				System.out.println("Failed to publish the event......");
-				System.exit(-1);
-			}
-		}
-	}
-	
-	private static String trimedValue(String value) {
-		if(value != null) {
-			return value.trim();
-		}
-		return value;
-	}
 
+		/**
+		 * Publishes the command 
+		 */
+
+		try {
+			// Generate a JSON object of the event to be published
+			JsonObject event = new JsonObject();
+			event.addProperty("reboot", 5);
+
+			boolean response = myClient.publishCommandOverHTTP("Cmd", event);
+			if (response == true)
+				System.out.println("Published Command Successfully!");
+			else
+				System.out.println("Failed Publishing Commad!");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
